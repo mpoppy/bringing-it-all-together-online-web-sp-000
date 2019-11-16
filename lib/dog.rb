bfa49cc771a995389c3713d2f70f9b1900353a39
Marc Poppy <poppy.marc@gmail.com>
2019-11-16 19:36:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,9 @@ class Dog
 
   attr_accessor :name, :breed, :id
 
-  @@all = []
-
   def initialize(name:, breed:, id:nil)
     @name = name
     @breed = breed
-    @@all << self
   end
 
   def self.create_table
